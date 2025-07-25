f4391a415394e287c874ef9d81c2cc95caa3f8b9
Anish Gharti <anishgc@proton.me>
2025-07-25 17:37:52 +0545

    looked generic views and authentication

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
+        
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
@@ -9,7 +17,7 @@ class SnippetSerializer(serializers.Serializer):
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    
+    owner = serializers.ReadOnlyField(source='owner.username')
     
     def create(self, validated_data):
         return Snippet.objects.create(**validated_data)
